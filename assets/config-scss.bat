a2471fde6c8cd558b2ee72898776e73f09941b5d
kwainoelawoffice <164697203+kwainoelawoffice@users.noreply.github.com>
2024-03-24 00:01:36 +0000

    Add files via upload

---
+++ assets/config-scss.bat
@@ -0,0 +1,2 @@
+cd C:\xampp\htdocs\Free Tempaltes\bootstrap-lawyer-template-free-download\assets
+scss --watch scss/style.scss:css/style.css
---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+
+      $(document).ready(function(){
+        $('.menu-toggle').click(function(){
+          $('.menu-toggle').toggleClass('active')
+          $('.my-nav-items').toggleClass('active')
+        });
+      });
+
